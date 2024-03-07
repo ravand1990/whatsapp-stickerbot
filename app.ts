ab0594ed23019e71176135ce39af5dec5f26ed3b
Ravand Atruschi <r.atruschi@optadata-gruppe.de>
2024-03-07 10:25:32 +0100

    fixed "to" "me" misunderstanding

--- app.ts
+++ app.ts
@@ -11,10 +11,13 @@ import * as qrcode from "qrcode-terminal";
 
 const Utils = require("whatsapp-web.js/src/util/Util.js");
 
+const isWin = process.platform === "win32";
+
 const options = {
   authStrategy: new LocalAuth(),
   puppeteer: {
-    executablePath: "/usr/bin/chromium",
+    channel: isWin ? "chrome" : undefined,
+    executablePath: isWin ? undefined : "/usr/bin/chromium",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
 } as ClientOptions;
@@ -33,7 +36,7 @@ client.on("message_create", (msg) => {
 });
 
 client.on("message", (msg: Message) => {
-  sendSticker(msg);
+  if (msg.fromMe) sendSticker(msg);
 });
 
 client.initialize();
@@ -47,7 +50,7 @@ async function sendSticker(msg: Message) {
     console.log(`Detected sticker request from ${msg.author}. Creating...`);
     const receivedMedia = await msg.downloadMedia();
     // let sticker = await convertToSticker(receivedMedia);
-    await client.sendMessage(msg.to, receivedMedia, {
+    await client.sendMessage(msg.fromMe ? msg.to : msg.from, receivedMedia, {
       sendMediaAsSticker: true,
     } as MessageSendOptions);
   }
