a87bfef80f006488377fbe946cebbe571b12e91c
Ravand Atruschi <r.atruschi@optadata-gruppe.de>
2024-03-12 13:09:30 +0100

    fix

--- app.ts
+++ app.ts
@@ -75,7 +75,7 @@ async function sendSticker(msg: Message) {
 
       msg.react("✅");
 
-      msg.reply(receivedMedia, msg.fromMe || isWin ? msg.to : msg.from, {
+      msg.reply(receivedMedia, msg.to, {
         stickerAuthor: "ravands_stickerbot",
         sendMediaAsSticker: true,
       });
